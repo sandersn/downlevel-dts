c007d4b457e0bad2855222ba104dcb7d494a967e
Nathan Shively-Sanders <293473+sandersn@users.noreply.github.com>
2020-01-16 14:38:49 -0800

    Work around triple slash duplication from Typescript (#13)
    Fixes #12
    Won't be needed after Microsoft/Typescript#36242 is fixed or has a
    better workaround.

--- baselines/ts3.4/test.d.ts
+++ baselines/ts3.4/test.d.ts
@@ -1,10 +1,11 @@
-/// <reference types="node" />
+/// <reference path="./src/test.d.ts" />
 /// <reference types="node" />
 export class C {
     protected p: number;
     public readonly q: string;
     private r: boolean;
 }
+// hi, this should still be there
 export namespace N {
     abstract class D {
         p: number;
